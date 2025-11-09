7437e3fec166b628282e458853ea80d23eef3a6c
Savo Savić <192821740+sav0sav1c5@users.noreply.github.com>
2025-11-09 23:35:48 +0100

    feat(schema_optimization): Add base functions for creating documents and collections

---
+++ src/database/optimization.py
@@ -0,0 +1,17 @@
+import time
+from pymongo import InsertOne
+
+def create_optimized_collections(database, batch_size = 10000):
+    """
+    Function for optimized migration with batch inserts and cathcing of data in RAM.
+    """
+
+    pass
+
+
+def build_optimized_documents(flights):
+    """
+    Function for building documents for optimized schema.
+    """
+
+    pass
