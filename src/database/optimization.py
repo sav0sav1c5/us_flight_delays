a6785504ac39753345ac88e148b6bc3f65dd7a90
Savo Savić <192821740+sav0sav1c5@users.noreply.github.com>
2025-11-11 16:07:10 +0100

    feat(schema_optimization): Add function for airport geolocation caching

--- src/database/optimization.py
+++ src/database/optimization.py
@@ -17,13 +17,31 @@ def cache_airports(database):
 
     return airports_cache
 
+def cache_airport_geolocations(database):
+    """
+    Function for caching airport geolocation data.
+    """
+    airport_geolocations_cache = {}
+
+    for geolocation in database['airports_geolocation'].find({}):
+
+        iata_code = geolocation.get('IATA_CODE')
+
+        if iata_code:
+            airport_geolocations_cache[iata_code.upper()] = geolocation
+
+    return airport_geolocations_cache
+
 def create_optimized_collections(database, batch_size = 10000):
     """
-    Function for optimized migration with batch inserts and cathcing of data in RAM.
+    Function for optimized migration with batch inserts and cachcing of data in RAM.
     """
 
     # Cache airport data
     airports_cache = cache_airports(database)
+
+    # Cache airport geolocation data
+    airports_geolocations_cache = cache_airport_geolocations(database)
 
     pass
 
