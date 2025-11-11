55efbb20b161f6181cb9a0e23925d0bc18b1cc3f
Savo Savić <192821740+sav0sav1c5@users.noreply.github.com>
2025-11-11 15:59:12 +0100

    feat(schema_optimization): Add function for airport caching

--- src/database/optimization.py
+++ src/database/optimization.py
@@ -1,10 +1,29 @@
 import time
 from pymongo import InsertOne
 
+def cache_airports(database):
+    """
+    Function for caching airport data.
+    """
+
+    airports_cache = {}
+
+    for airport in database['airports'].find({}):
+
+        iata_code = airport.get('iata_code')
+
+        if iata_code:
+            airports_cache[iata_code.upper()] = airport
+
+    return airports_cache
+
 def create_optimized_collections(database, batch_size = 10000):
     """
     Function for optimized migration with batch inserts and cathcing of data in RAM.
     """
+
+    # Cache airport data
+    airports_cache = cache_airports(database)
 
     pass
 
